67e5cd22464de016526f49fb166f4f4be87d9c71
ivan <ivan@archevod.localdomain>
2023-01-10 12:46:28 +0300

    Initial commit

---
+++ kursachtest/db/init-db.js
@@ -0,0 +1,206 @@
+db = db.getSiblingDB("test_db");
+db.test_db.drop();
+
+db.test_db.insertMany([
+    {
+        "id": 1,
+        "name": "Lion",
+        "type": "wild"
+    },
+    {
+        "id": 2,
+        "name": "Cow",
+        "type": "domestic"
+    },
+    {
+        "id": 3,
+        "name": "Tiger",
+        "type": "wild"
+    },
+    {
+        "id": 4,
+        "name": "Tiger",
+        "type": "wild"
+    },
+    {
+        "id": 5,
+        "name": "Lion",
+        "type": "wild"
+    },
+    {
+        "id": 6,
+        "name": "Cow",
+        "type": "domestic"
+    },
+    {
+        "id": 7,
+        "name": "Tiger",
+        "type": "wild"
+    },
+    {
+        "id": 8,
+        "name": "Tiger",
+        "type": "wild"
+    },
+    {
+        "id": 9,
+        "name": "Lion",
+        "type": "wild"
+    },
+    {
+        "id": 10,
+        "name": "Cow",
+        "type": "domestic"
+    },
+    {
+        "id": 11,
+        "name": "Tiger",
+        "type": "wild"
+    },
+    {
+        "id": 12,
+        "name": "Tiger",
+        "type": "wild"
+    },
+    {
+        "id": 13,
+        "name": "Lion",
+        "type": "wild"
+    },
+    {
+        "id": 14,
+        "name": "Cow",
+        "type": "domestic"
+    },
+    {
+        "id": 15,
+        "name": "Tiger",
+        "type": "wild"
+    },
+    {
+        "id": 16,
+        "name": "Tiger",
+        "type": "wild"
+    },
+    {
+        "id": 17,
+        "name": "Lion",
+        "type": "wild"
+    },
+    {
+        "id": 18,
+        "name": "Cow",
+        "type": "domestic"
+    },
+    {
+        "id": 19,
+        "name": "Tiger",
+        "type": "wild"
+    },
+    {
+        "id": 20,
+        "name": "Tiger",
+        "type": "wild"
+    },
+    {
+        "id": 21,
+        "name": "Lion",
+        "type": "wild"
+    },
+    {
+        "id": 22,
+        "name": "Cow",
+        "type": "domestic"
+    },
+    {
+        "id": 23,
+        "name": "Tiger",
+        "type": "wild"
+    },
+    {
+        "id": 24,
+        "name": "Tiger",
+        "type": "wild"
+    },
+    {
+        "id": 25,
+        "name": "Lion",
+        "type": "wild"
+    },
+    {
+        "id": 26,
+        "name": "Cow",
+        "type": "domestic"
+    },
+    {
+        "id": 27,
+        "name": "Tiger",
+        "type": "wild"
+    },
+    {
+        "id": 28,
+        "name": "Tiger",
+        "type": "wild"
+    },
+    {
+        "id": 29,
+        "name": "Lion",
+        "type": "wild"
+    },
+    {
+        "id": 30,
+        "name": "Cow",
+        "type": "domestic"
+    },
+    {
+        "id": 31,
+        "name": "Tiger",
+        "type": "wild"
+    },
+    {
+        "id": 32,
+        "name": "Tiger",
+        "type": "wild"
+    },
+
+    {
+        "id": 33,
+        "name": "Lion",
+        "type": "wild"
+    },
+    {
+        "id": 34,
+        "name": "Cow",
+        "type": "domestic"
+    },
+    {
+        "id": 35,
+        "name": "Tiger",
+        "type": "wild"
+    },
+    {
+        "id": 36,
+        "name": "Tiger",
+        "type": "wild"
+    },
+    {
+        "id": 37,
+        "name": "Lion",
+        "type": "wild"
+    },
+    {
+        "id": 38,
+        "name": "Cow",
+        "type": "domestic"
+    },
+    {
+        "id": 39,
+        "name": "Tiger",
+        "type": "wild"
+    },
+    {
+        "id": 40,
+        "name": "Tiger",
+        "type": "wild"
+    },
+]);
---
+++ kursachtest/pymongo/app.py
@@ -0,0 +1,35 @@
+import json
+from flask import Flask
+import pymongo
+from flask_caching import Cache 
+import redis
+
+app = Flask(__name__)
+
+class BaseConfig(object):
+    CACHE_TYPE='redis'
+    CACHE_REDIS_HOST='redis'
+    CACHE_REDIS_PORT='6379'
+    CACHE_REDIS_DB='0'
+    CACHE_REDIS_URL='redis://redis:6379/0'
+    CACHE_DEFAULT_TIMEOUT='500'
+
+    
+app.config.from_object(BaseConfig)
+cache = Cache(app)
+
+buf = {}
+myclient = pymongo.MongoClient("mongodb://192.168.0.51:27017/")
+mydb = myclient["test_db"]
+mycol = mydb["test_db"]
+     
+         
+@app.route("/")
+@cache.cached(timeout=30, query_string=True)
+def home_page():
+    for count, i in enumerate(mycol.find()):
+        buf[count] = i
+    return f'{buf}'
+
+if __name__ == "__main__":
+    app.run(debug=True, host='0.0.0.0', port='5000')
